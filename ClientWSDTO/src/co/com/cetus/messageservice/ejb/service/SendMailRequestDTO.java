d204b86856aa8d52d662a4bec1e60efb192757c7
Andrés Herrera <andres.herrera@cetustechnology.co>
2016-02-04 11:17:29 -0500

    se realizan ajustes a nivel de configuracion y a nivel de codigos

--- ClientWSDTO/src/co/com/cetus/messageservice/ejb/service/SendMailRequestDTO.java
+++ ClientWSDTO/src/co/com/cetus/messageservice/ejb/service/SendMailRequestDTO.java
@@ -1,8 +1,6 @@
 
 package co.com.cetus.messageservice.ejb.service;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
